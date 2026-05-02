e48400b778889260bf22a8992266d74b4bb90840
Gedeliah Hene Etone <jjetone@gmail.com>
2026-05-01 20:39:50 -0400

    s3 statefile

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "class-backend-bucket-2026"
+    bucket = "nullsector"
     key    = "state/terraform.tfstate"
     use_lockfile = true
     region = "us-east-1"
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "class" {
+/* resource "aws_s3_bucket" "class" {
   bucket = "nullsector"
 
   tags = {
     Name = local.ec2_tag
   }
-}
+} */
