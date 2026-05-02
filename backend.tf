6482e096b33a6c854362bbe89b723d6ffabce570
Gedeliah Hene Etone <jjetone@gmail.com>
2026-05-01 20:54:11 -0400

    Another s3 edit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nullsector"
+    bucket = "class-backend-bucket-2026"
     key    = "state/terraform.tfstate"
     use_lockfile = true
     region = "us-east-1"
